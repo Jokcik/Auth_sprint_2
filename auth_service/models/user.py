ca83b956c1362f9f2a13e62fbd7e916d05c0c0bb
d.cherenkov <jokcik@gmail.com>
2024-08-18 17:49:43 +0300

    add partition

--- auth_service/models/user.py
+++ auth_service/models/user.py
@@ -1,8 +1,10 @@
-from sqlalchemy import Boolean, Column, String, ForeignKey, Table, DateTime, func
-from sqlalchemy.orm import relationship
-from db.database import Base
-from sqlalchemy.dialects.postgresql import UUID
 import uuid
+
+from sqlalchemy import Boolean, Column, String, ForeignKey, Table, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
+
+from db.database import Base
 
 user_roles = Table('user_roles', Base.metadata,
                    Column('user_id', UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE")),
@@ -26,10 +28,13 @@ class User(Base):
 
 class LoginHistory(Base):
     __tablename__ = "loginhistory"
+    __table_args__ = {
+        'postgresql_partition_by': 'RANGE (login_at)',
+    }
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, index=True)
     user_id = Column(UUID(as_uuid=True), ForeignKey('users.id', ondelete="CASCADE"))
     user = relationship('User', back_populates='login_history')
     ip_address = Column(String)
     user_agent = Column(String)
-    login_at = Column(DateTime, default=func.now())
+    login_at = Column(DateTime, primary_key=True, default=func.now())
